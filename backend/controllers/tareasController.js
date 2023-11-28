b9edd15a101316d17855848fb4e39b9b3ca3e00c
EFREN NARVAEZ <efrennm86@gmail.com>
2023-11-28 13:17:33 -0600

    Rutas, controladores, errorHandler y asyncHandler

--- backend/controllers/tareasController.js
+++ backend/controllers/tareasController.js
@@ -1,14 +1,21 @@
-const  getTareas = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const  getTareas =asyncHandler( async (req, res) => {
     res.status(200).json({message:'OBTENER TAREAS'})
-}
+}) 
 
-const  setTareas = (req, res) => {
+const  setTareas =asyncHandler( async (req, res) => {
+    if(!req.body.texto){
+        res.status(400)
+        throw new Error("Por favor teclea una descripción")
+        
+    }
     res.status(201).json({message:'CREAR TAREAS'})
-}
+})
 
-const  modificarTareas = (req, res) => {
+const  modificarTareas =asyncHandler( async (req, res) => {
     res.status(200).json({message:`MODIFICAR LA TAREA ${req.params.id}` })
-}
+})
 
 module.exports ={
     getTareas, 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 
 /*En el archivo server no es correcto tener las rutas. Para ello tendremos nuestra carpeta de rutas
@@ -10,6 +11,12 @@ app.get('/api/tareas', (req, res)=>{
 })
 */
 
+
+//Estas líneas son para recibir info del body por post
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/tareas', require('./routes/tareasRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Servidor iniciado en puerto ${port}`)) 
